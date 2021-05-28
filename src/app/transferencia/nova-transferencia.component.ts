1093c2a80550d2a786eedf53d7dc3ad5cbece057
Rafael Mendes <rafael_mendes@outlook.com>
2021-05-27 21:06:35 -0300

    Finalização curso Angular

--- src/app/transferencia/nova-transferencia.component.ts
+++ src/app/transferencia/nova-transferencia.component.ts
@@ -2,6 +2,7 @@ import { Transferencia } from './../models/transferencia.model';
 import { EventEmitter } from '@angular/core';
 import { Component, Output } from '@angular/core';
 import { TransferenciaService } from '../services/transferencia.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -16,14 +17,15 @@ export class NovaTransferenciaComponent {
   destino: number = 0;
   data: number = Date.now();
 
-  constructor(private service: TransferenciaService){
+  constructor(private service: TransferenciaService, private router: Router){
 
   }
 
   transferir() {
     const transferencia: Transferencia = { valor: this.valor, destino: this.destino, data: this.data};
-    this.service.adicionar(transferencia).subscribe(resultado => {
+    this.service.adicionar(transferencia).subscribe( resultado => {
       this.limparCampos();
+      this.router.navigateByUrl('extrato');
     }, (error) => console.log(error));
   }
 
